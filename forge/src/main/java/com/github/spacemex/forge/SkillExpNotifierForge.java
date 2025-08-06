59c396d5473a938e0f23472ae0db1f9bbef92c6e
deven <devenshona@gmail.com>
2025-08-05 19:00:07 -0500

    Migrate `SkillExpNotifierForge` to NeoForge API and replace Forge networking with `RegisterPayloadHandlerEvent`.

--- forge/src/main/java/com/github/spacemex/forge/SkillExpNotifierForge.java
+++ forge/src/main/java/com/github/spacemex/forge/SkillExpNotifierForge.java
@@ -1,60 +1,52 @@
 package com.github.spacemex.forge;
 
 import com.github.spacemex.SkillExpNotifier;
+import com.github.spacemex.forge.networking.XpGainPayload;
 import com.github.spacemex.forge.networking.client.ClientNotifier;
 import com.github.spacemex.forge.networking.client.ToastPacketHandler;
 import com.github.spacemex.networking.XpGainPacket;
-import dev.architectury.platform.forge.EventBuses;
 import net.minecraft.util.Identifier;
-import net.minecraftforge.api.distmarker.Dist;
-import net.minecraftforge.api.distmarker.OnlyIn;
-import net.minecraftforge.eventbus.api.IEventBus;
-import net.minecraftforge.fml.DistExecutor;
-import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
-import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
-import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
-import net.minecraftforge.network.ChannelBuilder;
-import net.minecraftforge.network.SimpleChannel;
+import net.neoforged.api.distmarker.Dist;
+import net.neoforged.api.distmarker.OnlyIn;
+import net.neoforged.bus.api.IEventBus;
+import net.neoforged.fml.common.Mod;
+import net.neoforged.fml.event.lifecycle.FMLClientSetupEvent;
+import net.neoforged.fml.loading.FMLEnvironment;
+import net.neoforged.neoforge.network.event.RegisterPayloadHandlerEvent;
+import net.neoforged.neoforge.network.registration.IPayloadRegistrar;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 @Mod(SkillExpNotifier.MOD_ID)
 public final class SkillExpNotifierForge {
     public static Logger LOGGER = LoggerFactory.getLogger(SkillExpNotifier.MOD_ID);
-    public static SimpleChannel CHANNEL;
     public static final Identifier XP_GAIN_PACKET_ID = new Identifier(SkillExpNotifier.MOD_ID, "network");
-    public static final int PROTOCOL_VERSION = 1;
+    public static final String PROTOCOL_VERSION = "1";
 
-
-
-    public SkillExpNotifierForge() {
-        IEventBus eventBus = FMLJavaModLoadingContext.get().getModEventBus();
-        EventBuses.registerModEventBus(SkillExpNotifier.MOD_ID,eventBus);
+    public SkillExpNotifierForge(IEventBus eventBus) {
         SkillExpNotifier.init();
-        eventBus.addListener(this::onCommonSetup);
-        DistExecutor.unsafeRunWhenOn(Dist.CLIENT, ()-> ()-> {
+
+        eventBus.addListener(this::registerPayloadHandler);
+
+        if (FMLEnvironment.dist.isClient()){
             eventBus.addListener(this::clientInit);
-        });
+        }
+
+        LOGGER.info("SkillExpNotifier NeoForge module initialized");
+
     }
 
-    private void onCommonSetup(FMLCommonSetupEvent event) {
-        CHANNEL = ChannelBuilder.named(XP_GAIN_PACKET_ID)
-                .networkProtocolVersion(PROTOCOL_VERSION)
-                .clientAcceptedVersions((s,v)->true)
-                .serverAcceptedVersions((s,v)->true)
-                .simpleChannel();
+    private void registerPayloadHandler(RegisterPayloadHandlerEvent event){
+        final IPayloadRegistrar registrar = event.registrar(SkillExpNotifier.MOD_ID)
+                .versioned(PROTOCOL_VERSION);
 
-        CHANNEL.messageBuilder(XpGainPacket.class)
-                .encoder(XpGainPacket::encode)
-                .decoder(XpGainPacket::decode)
-                .consumerMainThread((msg, ctx)->{
-                    if (ctx.getDirection().getReceptionSide().isClient()){
-                        ToastPacketHandler.onXpGain(msg);
-                    }
-                    ctx.setPacketHandled(true);
+        registrar.play(XP_GAIN_PACKET_ID, XpGainPayload::new, handler -> handler
+                .client((payload, context) -> {
+                    context.workHandler().execute(() -> {
+                        ToastPacketHandler.onXpGain(new XpGainPacket(payload.getCategoryId(), payload.getDelta()));
+                    });
                 })
-                .add();
+        );
     }
     @OnlyIn(Dist.CLIENT)
     private void clientInit(FMLClientSetupEvent event) {
