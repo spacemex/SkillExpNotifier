e12fd894303730c1a99f672ebd52799b15c492ee
deven <devenshona@gmail.com>
2025-08-06 10:11:00 -0500

    Refactor `SkillExpNotifierFabricClient` to replace `XpGainPacket` with `XpGainPayload` and simplify global receiver implementation.

--- fabric/src/main/java/com/github/spacemex/fabric/client/SkillExpNotifierFabricClient.java
+++ fabric/src/main/java/com/github/spacemex/fabric/client/SkillExpNotifierFabricClient.java
@@ -2,9 +2,8 @@ package com.github.spacemex.fabric.client;
 
 import com.github.spacemex.SkillExpNotifier;
 import com.github.spacemex.client.XpToast;
-import com.github.spacemex.fabric.SkillExpNotifierFabric;
 import com.github.spacemex.fabric.networking.client.ClientNotifier;
-import com.github.spacemex.networking.XpGainPacket;
+import com.github.spacemex.networking.XpGainPayload;
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.fabric.api.client.networking.v1.ClientPlayNetworking;
 import net.fabricmc.fabric.api.client.rendering.v1.HudRenderCallback;
@@ -18,10 +17,9 @@ public final class SkillExpNotifierFabricClient implements ClientModInitializer 
             ClientNotifier.getToastComponent().render(drawContext);
         });
 
-        ClientPlayNetworking.registerGlobalReceiver(SkillExpNotifierFabric.XP_GAIN_PACKET_ID,(client,handler,buf,responseSender)->{
-            XpGainPacket pkt = XpGainPacket.decode(buf);
-            client.execute(()->{
-                ClientNotifier.getToastComponent().addToast(new XpToast(pkt.categoryId(),pkt.delta()));
+        ClientPlayNetworking.registerGlobalReceiver(XpGainPayload.ID,(payload,context)->{
+           context.client().execute(()->{
+                ClientNotifier.getToastComponent().addToast(new XpToast(payload.categoryId(),payload.delta()));
             });
         });
     }
