58aeab21aec7ee8390f6c086a9a96c3f092bbe5d
deven <devenshona@gmail.com>
2025-08-06 10:10:50 -0500

    Refactor `SkillExpNotifierFabric` to replace `XpGainPacket` with `XpGainPayload` and streamline packet handling with `PayloadTypeRegistry`.

--- fabric/src/main/java/com/github/spacemex/fabric/SkillExpNotifierFabric.java
+++ fabric/src/main/java/com/github/spacemex/fabric/SkillExpNotifierFabric.java
@@ -2,19 +2,16 @@ package com.github.spacemex.fabric;
 
 import com.github.spacemex.SkillExpNotifier;
 import com.github.spacemex.fabric.networking.ServerNotifier;
-import com.github.spacemex.networking.XpGainPacket;
+import com.github.spacemex.networking.XpGainPayload;
 import net.fabricmc.api.ModInitializer;
-import net.fabricmc.fabric.api.networking.v1.PacketByteBufs;
+import net.fabricmc.fabric.api.networking.v1.PayloadTypeRegistry;
 import net.fabricmc.fabric.api.networking.v1.ServerPlayNetworking;
-import net.minecraft.network.PacketByteBuf;
 import net.minecraft.server.network.ServerPlayerEntity;
-import net.minecraft.util.Identifier;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public final class SkillExpNotifierFabric implements ModInitializer {
     public static Logger LOGGER = LoggerFactory.getLogger(SkillExpNotifier.MOD_ID);
-    public static final Identifier XP_GAIN_PACKET_ID = new Identifier(SkillExpNotifier.MOD_ID, "network");
     @Override
     public void onInitialize() {
         SkillExpNotifier.init();
@@ -23,14 +20,11 @@ public final class SkillExpNotifierFabric implements ModInitializer {
     }
 
     private void registerPackets(){
-        ServerPlayNetworking.registerGlobalReceiver(XP_GAIN_PACKET_ID,(server,player,handler,buf,responseSender)-> server.execute(()->{
-
-        }));
+        PayloadTypeRegistry.playS2C().register(XpGainPayload.ID,XpGainPayload.CODEC);
     }
 
-    public static void sendXpGainPacket(ServerPlayerEntity player, XpGainPacket pkt){
-        PacketByteBuf buf = PacketByteBufs.create();
-        XpGainPacket.encode(pkt,buf);
-        ServerPlayNetworking.send(player, XP_GAIN_PACKET_ID, buf);
+    public static void sendXpGainPacket(ServerPlayerEntity player, XpGainPayload payload) {
+        ServerPlayNetworking.send(player, payload);
     }
+
 }
