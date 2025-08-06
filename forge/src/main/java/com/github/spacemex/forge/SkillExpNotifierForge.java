bdca527c72acf204aa5f7ecec2d8ed04d57a679a
deven <devenshona@gmail.com>
2025-08-06 10:11:12 -0500

    Refactor `SkillExpNotifierForge` to replace `XpGainPacket` with `XpGainPayload`, update payload handling with `RegisterPayloadHandlersEvent`, and streamline server registration.

--- forge/src/main/java/com/github/spacemex/forge/SkillExpNotifierForge.java
+++ forge/src/main/java/com/github/spacemex/forge/SkillExpNotifierForge.java
@@ -1,32 +1,29 @@
 package com.github.spacemex.forge;
 
 import com.github.spacemex.SkillExpNotifier;
-import com.github.spacemex.forge.networking.XpGainPayload;
+import com.github.spacemex.forge.networking.ServerNotifier;
 import com.github.spacemex.forge.networking.client.ClientNotifier;
 import com.github.spacemex.forge.networking.client.ToastPacketHandler;
-import com.github.spacemex.networking.XpGainPacket;
-import net.minecraft.util.Identifier;
+import com.github.spacemex.networking.XpGainPayload;
 import net.neoforged.api.distmarker.Dist;
 import net.neoforged.api.distmarker.OnlyIn;
 import net.neoforged.bus.api.IEventBus;
 import net.neoforged.fml.common.Mod;
 import net.neoforged.fml.event.lifecycle.FMLClientSetupEvent;
 import net.neoforged.fml.loading.FMLEnvironment;
-import net.neoforged.neoforge.network.event.RegisterPayloadHandlerEvent;
-import net.neoforged.neoforge.network.registration.IPayloadRegistrar;
+import net.neoforged.neoforge.network.event.RegisterPayloadHandlersEvent;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 @Mod(SkillExpNotifier.MOD_ID)
 public final class SkillExpNotifierForge {
     public static Logger LOGGER = LoggerFactory.getLogger(SkillExpNotifier.MOD_ID);
-    public static final Identifier XP_GAIN_PACKET_ID = new Identifier(SkillExpNotifier.MOD_ID, "network");
-    public static final String PROTOCOL_VERSION = "1";
 
     public SkillExpNotifierForge(IEventBus eventBus) {
         SkillExpNotifier.init();
 
-        eventBus.addListener(this::registerPayloadHandler);
+        ServerNotifier.register();
+        eventBus.addListener(this::registerPayloadHandlers);
 
         if (FMLEnvironment.dist.isClient()){
             eventBus.addListener(this::clientInit);
@@ -36,17 +33,19 @@ public final class SkillExpNotifierForge {
 
     }
 
-    private void registerPayloadHandler(RegisterPayloadHandlerEvent event){
-        final IPayloadRegistrar registrar = event.registrar(SkillExpNotifier.MOD_ID)
-                .versioned(PROTOCOL_VERSION);
-
-        registrar.play(XP_GAIN_PACKET_ID, XpGainPayload::new, handler -> handler
-                .client((payload, context) -> {
-                    context.workHandler().execute(() -> {
-                        ToastPacketHandler.onXpGain(new XpGainPacket(payload.getCategoryId(), payload.getDelta()));
-                    });
-                })
-        );
+    private void registerPayloadHandlers(RegisterPayloadHandlersEvent event) {
+        event.registrar(SkillExpNotifier.MOD_ID)
+                .playToClient(
+                        XpGainPayload.ID,
+                        XpGainPayload.CODEC,
+                        (payload, context) -> {
+                            context.enqueueWork(()-> {
+                                ToastPacketHandler.onXpGain(
+                                        new XpGainPayload(payload.categoryId(), payload.delta())
+                                );
+                            });
+                        }
+                );
     }
     @OnlyIn(Dist.CLIENT)
     private void clientInit(FMLClientSetupEvent event) {
