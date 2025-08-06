9796e8fd0b76524d90f00b0f3b593ebdc94bb927
deven <devenshona@gmail.com>
2025-08-06 13:42:02 -0500

    Update `ClientNotifier` to replace deprecated `HudRenderCallback` with `HudElementRegistry` for XP toast rendering.

--- fabric/src/main/java/com/github/spacemex/fabric/networking/client/ClientNotifier.java
+++ fabric/src/main/java/com/github/spacemex/fabric/networking/client/ClientNotifier.java
@@ -1,9 +1,11 @@
 package com.github.spacemex.fabric.networking.client;
 
+import com.github.spacemex.SkillExpNotifier;
 import com.github.spacemex.client.CustomToastComponent;
 import net.fabricmc.api.ClientModInitializer;
-import net.fabricmc.fabric.api.client.rendering.v1.HudRenderCallback;
+import net.fabricmc.fabric.api.client.rendering.v1.hud.HudElementRegistry;
 import net.minecraft.client.MinecraftClient;
+import net.minecraft.util.Identifier;
 
 public class ClientNotifier implements ClientModInitializer {
     private static final CustomToastComponent TOASTS = new CustomToastComponent(MinecraftClient.getInstance());
@@ -13,8 +15,12 @@ public class ClientNotifier implements ClientModInitializer {
     }
     @Override
     public void onInitializeClient() {
-        HudRenderCallback.EVENT.register((graphics, tickDelta) -> {
+        HudElementRegistry.addLast(Identifier.of(SkillExpNotifier.MOD_ID, "xp_toast_id"),
+                (context, tickCounter) -> TOASTS.render(context));
+
+       /** Removed In 1.20.6
+        *  HudRenderCallback.EVENT.register((graphics, tickDelta) -> {
             TOASTS.render(graphics);
-        });
+        });*/
     }
 }
